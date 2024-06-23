dbeb2f40a9321ff5b8404ae181cb0a8ed2fc302b
hyowonsong <woniwoni30@gmail.com>
2024-06-23 20:15:55 +0900

    first

---
+++ src/main/java/hello/item_service_web/ItemServiceWebApplication.java
@@ -0,0 +1,13 @@
+package hello.item_service_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItemServiceWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItemServiceWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hello/item_service_web/ItemServiceWebApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.item_service_web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItemServiceWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
